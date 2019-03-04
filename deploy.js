a478087c7b0778a8766a8f2e7a3d272d71c720bc
Jayy Lmao <jameswarringtonholman@gmail.com>
2019-03-04 21:51:50 +1100

    Can deploy but runs outta gas badly

--- deploy.js
+++ deploy.js
@@ -5,16 +5,16 @@ const { interface, bytecode } = require('./compile');
 
 const provider = new HDWalletProvider(
   'monster dentist auction dust traffic crawl sketch sick praise ghost include arch',
-  'https://rinkeby.infura.io/v3/9f6b7935e96a46d6a95e9cee4e4aaf8b'
+  'https://rinkeby.infura.io/v3/3906e495ccd24e6fb32613d0058d313b'
 );
 
 const web3= new Web3(provider);
 
+
 (async () => {
   const accounts = await web3.eth.getAccounts();
-  console.log('Attempting to deploy from account', accounts[0]);
   const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode })
+    .deploy({ data: bytecode, arguments: ['4'] })
     .send({gas: '1000000', from: accounts[0]});
   console.log(result.options.address);
 })();
