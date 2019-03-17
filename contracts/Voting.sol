478ba6829edd5553360e6b895cc59ec22b197c07
Jayy Lmao <jameswarringtonholman@gmail.com>
2019-03-18 10:41:11 +1100

    Finished, including extension ideas

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -54,6 +54,7 @@ contract Poll {
     function getResult() public view returns (bytes32) {
         require(q == numVoted);
         return tie ? bytes32(0x57696e6e65727320686176652074696564) : choiceNames[topChoiceIndex];
+        /* This string of bytes32 is an error message associated with tie-ing.*/
     }
 
     function destroy() public restricted {
@@ -70,18 +71,54 @@ contract Poll {
 
 
 /*
-Yet to implement:
-
-3. The contract creator is able to select m friends to vote for n 
-
-May need to make an array friendslist; then have it so that you can select/deselect people for pols
-
+================================================================================================================
 How would you extend the functionality of the smart contract? The description should appear
 as comments in the contract source code. (2 marks)
 • List out function definition, variables declaration, and pseudo-code
 • Explain why
+================================================================================================================
 
-One idea might be to have a timeout limit or something.
+How I would extend the smart contract:
+
+I would like to add a time-out so that if friends were unreliable, and only some voted, a winner would still be chosen.
+
+The check if the contract had timed-out would have to be performed in the `getResult` function. 
+To do this, change the `require(q == numVoted)` in the `getResult` function to also check timestamp and compare it to a new timeout variable, set by the creator. 
+The timeout variable would be set by the creator with a new `setTimeout` function, callable by only the creator (Using the restricted modifier). 
+This might not be a good fit for every blockchain voting DAPP, but in the case of something time-sensitive like lunch it is essential.
+Below I do not describe the exact conversion required, nor do I consider gas costs (it's to be considered solidity-flavored pseudocode)
+
+Variabes:
+```
+  uint public createdTimestamp;
+  uint public timeout;
+```
+
+New Function:
+```
+function setTimeout(uint newTimeout) public restricted {
+    tiemout = newTimeout;
+}
+```
+
+Changes to existing functions:
+
+```
+    function Poll(uint quorum) public {
+        ...
+        timeout = *a very large number, days worth even*
+        createdTimestamp = block.timestamp;
+        ...
+    }
+```
+
+```
+function getResult() public view returns (bytes32) {
+    ...
+    require(q == numVoted &&  createdTimestamp - now < timeout);
+    ...
+}
+```
 
 */
 
